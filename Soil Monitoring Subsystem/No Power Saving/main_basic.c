32e43183e05f365acfd41b304114ff51971c948b
ajayvarghese2000 <58085441+ajayvarghese2000@users.noreply.github.com>
2023-03-02 17:46:26 +0000

    Finished Soil Monitoring Subsystem No Power Saving

--- Soil Monitoring Subsystem/No Power Saving/main_basic.c
+++ Soil Monitoring Subsystem/No Power Saving/main_basic.c
@@ -1,7 +1,13 @@
 /**
  * @file    main_basic.c
  * @author  B929164 (Ajay Varghese)
- * @brief   This 
+ * @brief   This program is for the Soil Monitoring subsystem. It will be used to
+ *          monitor the soil moisture. If the soil moisture is above a certain
+ *          threshold, it will send a warning to the Zero. The program will wait
+ *          for the Zero to acknowledge the warning before continuing. The soil
+ *          moisture sensor will be connected to the Pi Pico via UART.
+ * 
+ *          This version utilises no power saving features.
  *          
 */
 
@@ -9,12 +15,16 @@
 
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
+#include "hardware/i2c.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 // ############################# [ Global Variables ] #############################
 
 // Pins Used on the Pi Pico
-const uint UART_TX_SOIL = 4;
-const uint UART_RX_SOIL = 5;
+const uint UART_TX_SOIL = 4;    // UART TX Pin for the Soil Sensor
+const uint UART_RX_SOIL = 5;    // UART RX Pin for the Soil Sensor
+uart_inst_t *uart_SOIL = uart1; // UART bus for the Soil Sensor
 
 
 const uint LED_PIN = 25;        // LED Pin for the Pi Pico
@@ -28,7 +38,6 @@ const uint ACK_PIN = 2;         // Acknowledge Pin for the Zero
 
 // ############################## [ Function Prototypes ] ##########################
 
-
 /**
  * @brief This sets up the warning pin and the acknowledge pin and sends a 
  * high signal to the warning pin. It then waits for the acknowledge pin to
@@ -39,6 +48,26 @@ const uint ACK_PIN = 2;         // Acknowledge Pin for the Zero
  * @return int 1 if successful 0 if failed
  */
 int issue_warning(uint WARNING_PIN, uint ACK_PIN);
+
+/**
+ * @brief Set the up soil sensor object
+ * 
+ * @param UART_TX_SOIL The UART TX pin that the RX pin of the soil sensor is connected to
+ * @param UART_RX_SOIL The UART RX pin that the TX pin of the soil sensor is connected to
+ * @param uart_SOIL    The UART bus that the soil sensor is connected to
+ * @return int         1 if successful 0 if failed
+ */
+int setup_soil_sensor(uint UART_TX_SOIL, uint UART_RX_SOIL, uart_inst_t *uart_SOIL);
+
+/**
+ * @brief Get the soil moisture object
+ * 
+ * @param UART_TX_SOIL The UART TX pin that the RX pin of the soil sensor is connected to
+ * @param UART_RX_SOIL The UART RX pin that the TX pin of the soil sensor is connected to
+ * @param uart_SOIL    The UART bus that the soil sensor is connected to
+ * @return int         The soil moisture value or -1 if failed
+ */
+int get_soil_moisture(uint UART_TX_SOIL, uint UART_RX_SOIL, uart_inst_t *uart_SOIL);
 
 
 int main() 
@@ -58,6 +87,29 @@ int main()
     gpio_init(ACK_PIN);
     gpio_set_dir(ACK_PIN, GPIO_IN);
 
+    // Setup the soil sensor
+    setup_soil_sensor(UART_TX_SOIL, UART_RX_SOIL, uart_SOIL);
+
+    // Get the soil moisture forever
+    while (1)
+    {
+        // Get the soil moisture if reading fails, try again
+        int soil_moisture = -1;
+        while (soil_moisture == -1)
+        {
+            soil_moisture = get_soil_moisture(UART_TX_SOIL, UART_RX_SOIL, uart_SOIL);
+        }
+
+        // print the soil moisture
+        printf("Soil Moisture: %d\r\n", soil_moisture);
+
+        // Check if the soil moisture is above the threshold
+        if (soil_moisture > 50)
+        {
+            // Issue a warning
+            issue_warning(WARNING_PIN, ACK_PIN);
+        }
+    }
     
 }
 
@@ -90,4 +142,98 @@ int issue_warning(uint WARNING_PIN, uint ACK_PIN)
     gpio_put(LED_PIN, 0);
 
     return 1;
+}
+
+
+
+int setup_soil_sensor(uint UART_TX_SOIL, uint UART_RX_SOIL, uart_inst_t *uart_SOIL)
+{
+    // Set up the UART bus
+    uart_init(uart_SOIL, 9600);
+    gpio_set_function(UART_TX_SOIL, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_SOIL, GPIO_FUNC_UART);
+
+    // Wait for the soil sensor to boot up
+    sleep_ms(2000);
+
+    // Send a l to the soil sensor to set it up
+    uart_putc_raw(uart_SOIL, 'l');
+
+    return 1;
+}
+
+int get_soil_moisture(uint UART_TX_SOIL, uint UART_RX_SOIL, uart_inst_t *uart_SOIL)
+{
+    // Send the command to get the soil moisture
+    uart_putc_raw(uart_SOIL, 'w');
+
+    // Wait for the response
+    sleep_ms(100);
+
+    // Get the response
+    char soil_moisture[8];
+
+    int soil_moisture_val = 0;
+
+    // Keep reading until a "=" is found
+    while (uart_getc(uart_SOIL) != '=')
+    {
+        // Do nothing
+    }
+
+    // Read in the next character will be the first digit of the soil moisture
+    soil_moisture[0] = uart_getc(uart_SOIL);
+
+    // Read in the next character will be the second digit of the soil moisture or a newline
+    soil_moisture[1] = uart_getc(uart_SOIL);
+
+    // Check if the character is a newline
+    if (soil_moisture[1]== '\n')
+    {
+        // Add the null terminator
+        soil_moisture[1] = '\0';
+
+        // Return the soil moisture
+        return atoi(soil_moisture);
+    }
+
+    // Read in the next character will be the third digit of the soil moisture or a newline
+    soil_moisture[2] = uart_getc(uart_SOIL);
+
+    // Check if the character is a newline
+    if (soil_moisture[2]== '\n')
+    {
+        // Add the null terminator
+        soil_moisture[2] = '\0';
+
+        // Return the soil moisture
+        return atoi(soil_moisture);
+    }
+    else if (soil_moisture[2]== '0')
+    {
+        // Add the null terminator
+        soil_moisture[3] = '\0';
+        
+        return atoi(soil_moisture);
+    }
+    
+
+    // Read in the next character
+    soil_moisture[3] = uart_getc(uart_SOIL);
+
+    // Check if the character is a newline
+    if (soil_moisture[3]== '\n')
+    {
+        // Add the null terminator
+        soil_moisture[3] = '\0';
+
+        // Return the soil moisture
+        return atoi(soil_moisture);
+    }
+
+    // Print error
+    printf("Error reading soil moisture\r\n");
+
+    // Return error
+    return -1;
 }
